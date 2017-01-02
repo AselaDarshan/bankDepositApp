df16b594a2e7bf9e980aff9eb162228db8fda4e3
buddhikajay <buddhika.12@cse.mrt.ac.lk>
2017-01-02 21:17:51 +0530

    Copied Basic Functions from Sample App

--- app/src/main/java/com/siplo/banking/bankdepositapp/PrintActivity.java
+++ app/src/main/java/com/siplo/banking/bankdepositapp/PrintActivity.java
@@ -1,13 +1,41 @@
 package com.siplo.banking.bankdepositapp;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
+import woyou.aidlservice.jiuiv5.ICallback;
+import woyou.aidlservice.jiuiv5.IWoyouService;
 
 public class PrintActivity extends AppCompatActivity {
+
+    private IWoyouService woyouService;
+    private ICallback callback = null;
+    private ServiceConnection connService = new ServiceConnection() {
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+            woyouService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            woyouService = IWoyouService.Stub.asInterface(service);
+            setButtonEnable(true);
+        }
+    };
+
+    private Button printButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +52,21 @@ public class PrintActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void initView(){
+
+        printButton = (Button) findViewById(R.id.printButton);
+
+        Intent intent=new Intent();
+        intent.setPackage("woyou.aidlservice.jiuiv5");
+        intent.setAction("woyou.aidlservice.jiuiv5.IWoyouService");
+        startService(intent);
+        bindService(intent, connService, Context.BIND_AUTO_CREATE);
+    }
+
+    private void setButtonEnable(){
+
     }
 
 }
