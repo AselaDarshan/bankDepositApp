3cc1ea5015b5d4e463e801fc014cb7ded0c039f7
buddhikajay <buddhika.12@cse.mrt.ac.lk>
2017-01-02 21:41:25 +0530

    Created On Click Listener

--- app/src/main/java/com/siplo/banking/bankdepositapp/PrintActivity.java
+++ app/src/main/java/com/siplo/banking/bankdepositapp/PrintActivity.java
@@ -44,12 +44,11 @@ public class PrintActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        initView();
+        printButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                test();
             }
         });
     }
@@ -66,6 +65,10 @@ public class PrintActivity extends AppCompatActivity {
     }
 
     private void setButtonEnable(boolean flag){
+
+    }
+
+    private void test(){
 
     }
 
